cb2002711cd002f2cff861586d9ccd623b4a7a66
bruno <bruno@9760f4c4-1700-0410-9533-feba0de264b8>
2010-06-24 00:22:04 +0000

    r3927@localhost:  bruno | 2010-06-23 02:13:19 +0200  - Improved version for log file mngt

--- devel/mrmini/lib/MondoRescue/Mini/Base.pm
+++ devel/mrmini/lib/MondoRescue/Mini/Base.pm
@@ -50,7 +50,6 @@ Main function for mini. Can be called from outside
 
 sub mr_mini_main {
 
-	my $logfile = shift || undef;
 #
 # Configuration parameters
 #
@@ -64,38 +63,35 @@ chomp($ARCH);
 pb_conf_add("$mr->{'confdir'}/$ENV{'PBPROJ'}.conf.dist");
 ($mr->{'boot_size'},$mr->{'boot_cd'},$mr->{'boot_usb'},$mr->{'boot_tape'},$mr->{'kernel'},$mr->{'fstab'}) =  mr_conf_get("mr_boot_size","mr_boot_cd","mr_boot_usb","mr_boot_tape","mr_kernel","mr_fstab");
 ($mr->{'tape_mods'},$mr->{'scsi_mods'},$mr->{'ide_mods'},$mr->{'pcmcia_mods'},$mr->{'usb_mods'},$mr->{'net_mods'},$mr->{'cdrom_mods'},$mr->{'deny_mods'},$mr->{'force_mods'}) =  mr_conf_get("mr_tape_mods","mr_scsi_mods","mr_ide_mods","mr_pcmcia_mods","mr_usb_mods","mr_net_mods","mr_cdrom_mods","mr_extra_mods","mr_deny_mods","mr_force_mods");
-($mr->{'log_file'},$mr->{'cache_dir'},$mr->{'boot_msg'},$mr->{'burn_cmd'},$mr->{'burn_opt'}) = mr_conf_get("mr_log_file","mr_cache_dir","mr_boot_msg","mr_burn_cmd","mr_burn_opt");
+($mr->{'cache_dir'},$mr->{'boot_msg'},$mr->{'burn_cmd'},$mr->{'burn_opt'}) = mr_conf_get("mr_cache_dir","mr_boot_msg","mr_burn_cmd","mr_burn_opt");
 
 #
 # Manage log file
 #
-$logfile = $mr->{'log_file'} if (not defined $logfile);
-
-open(pbLOG,"> $logfile") || die "Unable to log to $logfile: $!";
-$pbLOG = \*pbLOG;
+$pbLOG = $mr->{'logdesc'};
 $pbdebug = 1  if ($pbdebug == -1);
 pb_log_init($pbdebug, $pbLOG);
 
-pb_log(0,"$ENV{'PBPKG'} start date: $mr->{'start_date'}\n");
-pb_log(0,"-------------------------------------\n");
-pb_log(0,"$ENV{'PBPKG'} v$mr->{'version'}\n");
+my $sep = "-----------------------------------------------\n";
+
+pb_log(0,"$ENV{'PBPKG'} v$mr->{'version'} start date: $mr->{'start_date'}\n");
+pb_log(0,$sep);
 pb_log(0,"$ARCH architecture detected\n");
 pb_log(0,"$ENV{'PBPKG'} called with the following arguments: ".join(" ",@ARGV)."\n");
-pb_log(0,"-------------------------------------\n");
-pb_log(0,"MONDO_CACHE: $ENV{'MONDO_CACHE'}\n") if (defined $ENV{'MONDO_CACHE'});
-pb_log(0,"MRMINI CONFDIR: $mr->{'confdir'}\n");
-pb_log(0,"MRMINI SBIN: $mr->{'install_dir'}/sbin\n");
+pb_log(0,$sep);
+pb_log(1,"CONFDIR: $mr->{'confdir'}\n");
+pb_log(1,"SBIN: $mr->{'install_dir'}/sbin\n");
 if (-r "$ENV{'HOME'}/.mondorescuerc") {
-	pb_log(0,"-------------------------------------\n");
+	pb_log(0,$sep);
 	pb_log(0,"Conf file $ENV{'HOME'}/.mondorescuerc\n");
-	pb_display_file("$ENV{'HOME'}/.mondorescuerc");
+	pb_display_file("$ENV{'HOME'}/.mondorescuerc",$pbLOG);
 }
 if (-r "$mr->{'confdir'}/mondorescue.conf") {
-	pb_log(0,"-------------------------------------\n");
+	pb_log(0,$sep);
 	pb_log(0,"Conf file $mr->{'confdir'}/mondorescue.conf\n");
-	pb_display_file("$mr->{'confdir'}/mondorescue.conf");
+	pb_display_file("$mr->{'confdir'}/mondorescue.conf",$pbLOG);
 }
-pb_log(0,"-------------------------------------\n");
+pb_log(0,$sep);
 
 #
 # Prepare cache dir
@@ -112,6 +108,5 @@ my $mrmini_deplist = "$mr->{'confdir'}/deplist.d";
 my ($lvmver,$lvmcmd) = mr_lvm_check();
 
 pb_log(0,"LVM $lvmver command set to $lvmcmd\n");
-pb_log(0,"-------------------------------------\n");
-close(pbLOG);
+pb_log(0,$sep);
 }
